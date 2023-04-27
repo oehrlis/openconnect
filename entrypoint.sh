b13086ab396ab1e3ae4ed8d791a5a5106f30f59a
Stefan Oehrli <stefan.oehrli@accenture.com>
2023-04-27 06:06:07 +0200

    Create and set password for vpn user

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,11 +27,11 @@ echo "USER_UID: "${USER_UID:-9001}
 # generate host keys if not present
 ssh-keygen -A
 
-ROOT_PWD=$(pwgen -1 15)
+VPN_PWD=$(pwgen -1 15)
 echo "--------------------------------------"
-echo "- root password : $ROOT_PWD"
+echo "- vpn password : $VPN_PWD"
 echo "--------------------------------------"
-echo "root:$ROOT_PWD" | chpasswd
+echo "vpn:$VPN_PWD" | chpasswd
 
 # do not detach (-D), log to stderr (-e)
 exec /usr/sbin/sshd -D -e &
